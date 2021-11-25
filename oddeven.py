e00cea18fafe166a2ee71d29e1f84e5b645be854
SyamSkyLab <syamtechy5858@gmail.com>
2021-11-25 14:51:10 +0530

    first commit

---
+++ oddeven.py
@@ -0,0 +1,6 @@
+x=int(input("Enter a number"))
+if(x%2==0):
+         print("Given number is even")
+else:
+    
+        print("Given number is odd")
---
+++ posneg.py
@@ -0,0 +1,6 @@
+x=int(input("Enter a number"))
+if x>0:
+      print("Positive number")
+else:
+     print("Negative number")
+      
